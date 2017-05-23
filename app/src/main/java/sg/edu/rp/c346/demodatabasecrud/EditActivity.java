f20c74ecc8b369604c465cfeb97faee08200ea4e
15056158 <15056158@myrp.edu.sg>
2017-05-23 13:13:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demodatabasecrud/EditActivity.java
@@ -0,0 +1,66 @@
+package sg.edu.rp.c346.demodatabasecrud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        tvID = (TextView) findViewById(R.id.tvID);
+        etContent = (EditText)findViewById(R.id.etContent);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+
+                Toast.makeText(EditActivity.this, "UPDATE SUCCESSFUL",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                dbh.close();
+
+                Toast.makeText(EditActivity.this, "DELETE SUCCESSFUL",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+
+}
